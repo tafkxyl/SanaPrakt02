f4dcffad7c051d162e74f0939b89114400f38631
Tania Zoshchak <ipz213_zta@student.ztu.edu.ua>
2022-11-08 00:42:18 +0200

    changed lest 2 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,6 +2,6 @@
 Console.WriteLine("Please understand that I'm trying my hardest");
 Console.WriteLine("My head's a mess but I'm trying regardless");
 Console.WriteLine("Anxiety is one hell of a problem");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("She's latching onto me, I can't resolve it");
+Console.WriteLine("It's not right, it's not fair, it's not fair, it's not fair, it's no fair, it no fair");
 Console.ReadKey();
