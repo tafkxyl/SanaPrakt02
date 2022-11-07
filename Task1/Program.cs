64ff20f4566075c804c40366760e54cda592d163
Tania Zoshchak <ipz213_zta@student.ztu.edu.ua>
2022-11-08 00:40:11 +0200

    changed first 3 lines

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.Text;
-
-Console.OutputEncoding = Encoding.UTF8;
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Please understand that I'm trying my hardest");
+Console.WriteLine("My head's a mess but I'm trying regardless");
+Console.WriteLine("Anxiety is one hell of a problem");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.ReadKey();
