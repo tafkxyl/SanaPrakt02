e99525f0b6cf436649f24dca706c190d37049633
Tania Zoshchak <ipz213_zta@student.ztu.edu.ua>
2022-11-07 23:44:05 +0200

    clone line in project task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -3,4 +3,8 @@ using System.Text;
 
 Console.OutputEncoding = Encoding.UTF8;
 Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
 Console.ReadKey();
