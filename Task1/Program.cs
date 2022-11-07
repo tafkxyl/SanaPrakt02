c9aaa1cb27e56ffd91f6d3612c90f4d8aa474719
Tania Zoshchak <ipz213_zta@student.ztu.edu.ua>
2022-11-07 23:30:29 +0200

    changed text in project

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.ReadKey();
